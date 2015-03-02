f7f02b3cc7a5b694de9512eeb1adcc6c9e255025
William Showalter <williamshowalter@gmail.com>
2015-03-02 14:07:01 -0900

    Initial Commit

---
+++ BattleFleet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BattleFleet
+//
+//  Created by William Showalter on 15/03/02.
+//  Copyright (c) 2015 UAF CS372. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
